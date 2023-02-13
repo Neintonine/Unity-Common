abdfa2ed44f9dcd1eea35c51fa901cb8cf82fa47
Michel Fedde <michel.fedde@gmail.com>
2023-02-14 00:09:59 +0100

    Moved extensions to own files

---
+++ Runtime/Scripts/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Common.Runtime.Extensions
+{
+    public static class GameObjectExtensions
+    {
+        public static void SetLayerRecursively(this GameObject obj, int layer, params int[] ignoreLayers)
+        {
+            if (!ignoreLayers.Contains(obj.layer)) obj.layer = layer;
+            foreach (Transform t in obj.transform)
+            {
+                SetLayerRecursively(t.gameObject, layer, ignoreLayers);
+            }
+        }
+
+        public static bool CheckLayersOR(this GameObject obj, params int[] layers)
+        {
+            for (int i = 0; i < layers.Length; i++)
+            {
+                if (layers[i] == obj.layer) return true;
+            }
+
+            return false;
+        }
+
+        public static bool CheckLayerAND(this GameObject obj, params int[] layers)
+        {
+            return layers.All(a => a == obj.layer);
+        }
+        
+        public static GameObject SearchGameObject(this GameObject obj, string name)
+        {
+            return obj.transform.SearchGameObjectTransform(name).gameObject;
+        }
+    }
+}
---
+++ Runtime/Scripts/Extensions/VisualEffectAssetExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+namespace Common.Runtime.Extensions
+{
+    public static class VisualEffectAssetExtensions
+    {
+        public static void Spawn(this VisualEffectAsset effect, Vector3 position, float? decay = 5)
+        {
+            GameObject effectHolder = new GameObject("_" + effect.name);
+            effectHolder.transform.position = position;
+            effectHolder.AddComponent<VisualEffect>().visualEffectAsset = effect;
+            if (decay.HasValue) GameObject.Destroy(effectHolder, decay.Value);
+        }
+    }
+}
