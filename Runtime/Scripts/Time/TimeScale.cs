6c833254dd41f65ddf02d7ff0f766d3126d58bb9
Michel Fedde <michel.fedde@gmail.com>
2023-02-14 00:06:44 +0100

    Added TimeScale utility classes

---
+++ Runtime/Scripts/Time/TimeScale.cs
@@ -0,0 +1,24 @@
+﻿using Codice.Client.BaseCommands;
+
+namespace Common.Runtime.Time
+{
+    public class TimeScale
+    {
+        public static UnityTimeScale UnityScale = new UnityTimeScale();
+
+        public virtual float Scale
+        {
+            get => _scale;
+            set => _scale = value;
+        }
+        public virtual float DeltaTime => UnityEngine.Time.deltaTime * Scale;
+        public virtual float FixedDeltaTime => UnityEngine.Time.fixedDeltaTime * Scale;
+        
+        private float _scale;
+        
+        public TimeScale(float scale = 1f)
+        {
+            _scale = scale;
+        }
+    }
+}
---
+++ Runtime/Scripts/Time/UnityTimeScale.cs
@@ -0,0 +1,18 @@
+﻿namespace Common.Runtime.Time
+{
+    public sealed class UnityTimeScale : TimeScale
+    {
+        public static float UnscaledDeltaTime => UnityEngine.Time.unscaledDeltaTime;
+        public static float UnscaledFixedDeltaTime => UnityEngine.Time.fixedUnscaledDeltaTime;
+        public static float Time => UnityEngine.Time.time;
+        public static float FixedTime => UnityEngine.Time.fixedTime;
+        
+        public override float Scale
+        {
+            get => UnityEngine.Time.timeScale;
+            set => UnityEngine.Time.timeScale = value;
+        }
+        public override float DeltaTime => UnityEngine.Time.deltaTime;
+        public override float FixedDeltaTime => UnityEngine.Time.fixedDeltaTime;
+    }
+}
